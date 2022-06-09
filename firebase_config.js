2c3413781edc6875b9aff317733d13c60ca904a1
Pranay Prangan <86947553+Pranay5463@users.noreply.github.com>
2022-06-09 17:40:53 +0530

    From PC to GitHub by @Pranay5463
    Uploaded directly from PC to GitHub.
    
    PC - D://Downloads/Firecdn-main
    GitHub - https://github.com/Pranay5463/firecdn
    
    Credits : @Pranay5463
    
    Contact me : http://t.me/dark_elite_98

---
+++ firebase_config.js
@@ -0,0 +1,18 @@
+/* Copy and Paste Firebase Config like below, Make sure it is named as "firebaseConfig" */
+
+/*---------------------------Replace------------------------------*/
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "yourappname.firebaseapp.com",
+  projectId: "yourappname",
+  storageBucket: "yourappname.appspot.com",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+/*---------------------------Replace------------------------------*/
+
+
+firebase.initializeApp(firebaseConfig); /* Initialize the Firebase*/
